c05fbe1871d025e2562f876cee4bdfd0edf626e0
visualstudio <visualstudio@230-PC20>
2019-03-07 17:28:51 -0500

    lab09

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -2,36 +2,14 @@
 //.C0731788
 //Assignment2
 //March 07,2019
-using System;
-
-namespace DelegatesAndEvents
+namespace ConsoleApp
 {
-    public class Program
-    {
-        public static void Main()
-        {
-            DelegateExercises delegateExercises = new DelegateExercises();
-            delegateExercises.Method3();
-            Console.ReadLine();
-        }
-    }
-
 
     public delegate void MyDelegate();
 
-    public class DelegateExercises
+    public class DelegateExercises : MyDelegate
     {
-        void Method1()
-        {
-            System.Console.WriteLine("Method1");
 
-        }
-        public void Method3()
-        {
-            MyDelegate myDelegate = new MyDelegate(Method1);
-            myDelegate();
-        }
     }
-
 }
 

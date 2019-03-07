9a313b76a1de03f6def9712630a2716306b8e745
visualstudio <visualstudio@230-PC20>
2019-03-07 17:36:46 -0500

    lab13

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -2,47 +2,36 @@
 //.C0731788
 //Assignment2
 //March 07,2019
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ConsoleApp
+namespace anmol
 {
-    using System;
-    delegate void ExampleDelegte(string xyz);
-    class Program
+
+    public class Program
     {
-        public static void Method1(string xyz)
-        {
-            Console.WriteLine(xyz + " Method1");
-        }
-        public static void Method2(string xyz)
-        {
-            Console.WriteLine(xyz + " Method2");
-        }
-
-
-
         public static void Main()
         {
-            ExampleDelegte ex1Delegate, ex2Delegate, ex3Delegate, myDelegate;
-            ex1Delegate = new ExampleDelegte(Method1);
-            ex2Delegate = new ExampleDelegte(Method2);
-            ex3Delegate = ex1Delegate + ex2Delegate;
-            myDelegate = ex1Delegate - ex2Delegate;
-            ex1Delegate("AAA");
-            ex2Delegate("AAA");
-            ex3Delegate("CCC");
-            myDelegate("DDD");
-            myDelegate = ex3Delegate - ex1Delegate;
-            myDelegate("EEE");
-            myDelegate = ex3Delegate - ex2Delegate;
-            myDelegate("FFF");
+            DelegateExercises delegateExercises = new DelegateExercises();
+            delegateExercises.Method3();
             Console.ReadLine();
+        }
+    }
+    public delegate int MyDelegate(out int i);
+
+    public class DelegateExercises
+    {
+        int Method1(out int i)
+        {
+            System.Console.WriteLine("Method1");
+            i = 10;
+            return 0;
+        }
+        public void Method3()
+        {
+            MyDelegate myDelegate = new MyDelegate(Method1);
+            MyDelegate myDelegate1 = new MyDelegate(Method1);
+            MyDelegate myDelegate2 = myDelegate + myDelegate1;
+            int intValue;
+            myDelegate2(out intValue);
         }
 
     }
 }
-

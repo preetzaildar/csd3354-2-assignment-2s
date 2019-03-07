16550b64dc4652737c3a37a89cc541c06a4d72f8
visualstudio <visualstudio@230-PC20>
2019-03-07 17:33:00 -0500

    lab11

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -2,38 +2,18 @@
 //.C0731788
 //Assignment2
 //March 07,2019
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace ConsoleApp
 {
-    public class Program
-    {
-        public ststic void Main()
-        {
 
-            DelegateExercises delegateExercises = new DelegateExercises();
-            DelegateExercises.Method3();
-            Console.ReadLine();
-        }
-    }
 
     public delegate void MyDelegate();
 
     public class DelegateExercises
     {
-        void Method1()
+        void Method3()
         {
-            System.Console.WriteLine("Method1");
-        }
-        public void Method3()
-        {
-            MyDelegate myDelegate = new MyDelegate(Method1);
-            myDelegate();
-            System.Console.WriteLine(myDelegate.ToString());
+
+            System.Console.WriteLine(MyDelegate.ToString());
         }
 
     }

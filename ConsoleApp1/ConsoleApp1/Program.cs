794bc8efb98f9e360a47c96ecc512c020c6eefa1
visualstudio <visualstudio@230-PC20>
2019-03-07 17:00:35 -0500

    lab06

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -3,12 +3,8 @@
 //Assignment2
 //March 07,2019
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace ConsoleApp
+namespace DelegatesAndEvents
 {
     public class Program
     {
@@ -16,19 +12,34 @@ namespace ConsoleApp
         {
             DelegateExercises delegateExercises = new DelegateExercises();
             delegateExercises.Method3();
+            Console.ReadLine();
         }
     }
     public class DelegateExercises
     {
-        public delegate int MyDelegate();
-        void Method1()
+        public delegate int MyDelegate(int intValue);
+        int Method1(int intMethod1)
         {
-            System.Console.WriteLine("MyDelgate");
+            return intMethod1 * 2;
         }
+
+        int Method2(int intMethod1)
+        {
+            return intMethod2 * 10;
+        }
+
+        public void Method4(MyDelegate myDelegate)
+        {
+            int result = myDelegate(10);
+            Console.WriteLine(result);
+        }
+
         public void Method3()
         {
             MyDelegate myDelegate = new MyDelegate(Method1);
-            myDelegate();
+            Method4(myDelegate);
+            myDelegate = new MyDelegate(Method2);
+            Method4(myDelegate);
         }
     }
 }
